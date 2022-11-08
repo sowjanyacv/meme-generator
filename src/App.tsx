acdfc4f270dafcdcebc714cb94556aa7d04813dd
nia <nia@notespace.jp>
2022-11-08 14:49:34 +0900

    Header Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+
+import { TheHeader } from "./components/TheHeader";
+
+import { theme } from "./styles/theme";
+import "./styles/fonts.css";
 
 export const App = () => {
   return (
-    <Box textAlign="center" fontSize="xl">
-      Hello
-    </Box>
+    <Flex
+      dir="column"
+      flexWrap="wrap"
+      m="0"
+      width="550px"
+      background={theme.colors.white}
+      fontFamily="'Kalam', cursive"
+      color={theme.colors.black}
+      gap="0px"
+    >
+      <TheHeader />
+    </Flex>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import * as React from "react";
+import { Flex } from "@chakra-ui/react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { theme } from "./styles/theme";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  <React.StrictMode>
+  <Flex
+    height="100vh"
+    width="100vw"
+    background={theme.colors.grey}
+    justifyContent="center"
+  >
     <App />
-  </React.StrictMode>
+  </Flex>
 );
